1b9981d98a0e50fc9ef28850923cdd6efd2fe0df
amitkumarnandi92 <amitkumarnandi92@gmail.com>
2024-10-19 21:23:27 +0600

    finished project

---
+++ app/src/main/java/com/amittech/notesapp/data/NotesDatabase.kt
@@ -0,0 +1,13 @@
+package com.amittech.notesapp.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+abstract class NotesDatabase: RoomDatabase(){
+    abstract val dao: NoteDao
+}
---
+++ app/src/main/java/com/amittech/notesapp/presentation/NoteState.kt
@@ -0,0 +1,13 @@
+package com.amittech.notesapp.presentation
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import com.amittech.notesapp.data.Note
+
+data class NoteState(
+
+    val notes: List<Note> = emptyList(),
+    val title: MutableState<String> = mutableStateOf(""),
+    val description: MutableState<String> = mutableStateOf("")
+
+)
